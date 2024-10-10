8e8ebbdc1d11aaeebb078926588959b821517231
LennartGj <lennartgjaltema@gmail.com>
2024-10-10 13:03:30 +0200

    Timers done

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include <stdint.h>
 
 void initTimer0();
 void initTimer1();
@@ -61,8 +62,6 @@ void initTimer1()
 // timer1 mode 4, CTC, om bij 13500 timer ticks interupt te gooien en dan te resetten naar 0;
 // OCR1A 13500. \n
 // OCR1AH = 00110100, OCR1AL = 10111100.
-// TOV1 moet high zijn om interrupt flag te enablen dus: \n
-// TIFR1 = 00000010;
 // WGM13 0, WGM12(CTC1) 1, WGM11(PWM11) 0, WGM10(PWM10) 0
 // prescaler 1024 --> CS12 1, CS11 0, CS10 1;
 // Set OCIE1A voor output compare A Match interupt enable
@@ -70,5 +69,11 @@ void initTimer1()
 {
   TCCR1B |= (1<<WGM12)|(1<<CS12)|(0<<CS11)|(1<<CS10);
   OCR1A = 13500;
+<<<<<<< Updated upstream
 }
 // INTERUPT AANZETTEN
+=======
+  TIMSK1 |= (1<<OCIE1A);
+  // TIFR1 |= (1<<OCF1A); geen flag nodig omdat er al een interupt wordt gegooid 
+}
+>>>>>>> Stashed changes
