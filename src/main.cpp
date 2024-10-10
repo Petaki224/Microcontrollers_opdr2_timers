a3c93746b872e0cc3b0cd065b5969f1c513ad6fe
LennartGj <lennartgjaltema@gmail.com>
2024-10-10 15:17:13 +0200

    Werkende display_centibeats
    Inclusief switchcase

--- src/main.cpp
+++ src/main.cpp
@@ -3,19 +3,81 @@
 
 void initTimer0();
 void initTimer1();
+void display_centibeats(uint8_t centibeats);
 
 volatile uint8_t centiBeatsCounted = 0;
 volatile uint8_t miliseconds = 0;
+const int PCF8574_address = 0x21;
+uint8_t prevCentiBeat = 0;
+
 
 ISR(TIMER1_COMPA_vect)
 {
     centiBeatsCounted++;
-    PORTB ^= (1<<PB5);
+    // display_centibeats(centiBeatsCounted);
 }
 
 void display_centibeats(uint8_t centibeats){
-
-
+  Wire.beginTransmission(PCF8574_address);
+  // Wire.write(centibeats);
+  Serial.println(centibeats);
+  switch (centibeats)
+  {
+  case 0:
+    Wire.write(0xC0);
+    break;
+  case 1:
+    Wire.write(0xF9);
+    break;
+  case 2:
+    Wire.write(0xA4);
+    break; 
+  case 3:
+    Wire.write(0xB0);
+    break;
+  case 4:
+    Wire.write(0x99);
+    break;
+  case 5:
+    Wire.write(0x92); // 5 getest
+    break;
+  case 6:
+    Wire.write(0x82);
+    break;
+  case 7:
+    Wire.write(0xF8);
+    break;
+  case 8:
+    Wire.write(0x80);
+    break;
+  case 9:
+    Wire.write(0x90);
+    break;
+  case 10:
+    Wire.write(0b10001000);
+    break;
+  case 11:
+    Wire.write(0b10000011);
+    break;
+  case 12:
+    Wire.write(0b11000110);
+    break;
+  case 13:
+    Wire.write(0b10100001);
+    break;
+  case 14:
+    Wire.write(0b10000110);
+    break;
+  case 15:
+    Wire.write(0b10001110);
+    break;
+    
+  default:
+    Wire.write(0x00);
+    break;
+  }
+  
+  Wire.endTransmission();
 
 }
 
@@ -33,11 +95,27 @@ int main(void){
   DDRB |= (1<<PB5);
 
   sei();
+  Wire.begin();
+  // Wire.beginTransmission(PCF8574_address);
+  // Wire.write(0x02);
+  // Wire.endTransmission();
   Serial.begin(9600);
   Serial.write("Ready");
   
-  while (true){
-
+  while (true){   
+    if (centiBeatsCounted>15)
+    {
+      centiBeatsCounted = 0;
+      prevCentiBeat = 0;
+      display_centibeats(centiBeatsCounted);
+    }
+    if (centiBeatsCounted > prevCentiBeat && (centiBeatsCounted <16))
+      {
+      display_centibeats(centiBeatsCounted);
+      prevCentiBeat = centiBeatsCounted;
+      }
+    
+    
 
 
 
